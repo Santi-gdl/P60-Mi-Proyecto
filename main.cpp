e6710d269a1fd6e9a1d357e982ed3d4adad25728
Santiago Pahuanquiza <gpahuanquiza@est.ups.edu.ec>
2022-05-31 10:34:37 -0500

    Creada la clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "persona.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Mi primer proyecto en Git y GitHub" << endl;
+    Persona p;
+    p.nombre="Pedro";
+    p.edad= 39;
+    cout << p.nombre << endl;
+    cout << p.edad << endl;
     return 0;
 }
